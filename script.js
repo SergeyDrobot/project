45631faa152f82f8ee86d21d370f4f6a2d7db1a2
Sergey <kyubifox9@gmail.com>
2021-03-15 10:32:54 +0200

    1 домашнее задание

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+
+const nubmerOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+
+const personalMovieDB = {
+    count: nubmerOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+const a = prompt("Один из последних просмотреных фильмов?", ''),
+      b = prompt("На сколько оцените его?", ''),
+      c = prompt("Один из последних просмотреных фильмов?", ''),
+      d = prompt("На сколько оцените его?", '');
+
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
